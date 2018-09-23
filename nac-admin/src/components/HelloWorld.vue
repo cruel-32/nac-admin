5ad54d9f404953d07d0f3ad41d721c4adaedfcae
cruel32 <cshee8508@gmail.com>
2018-09-23 19:16:07 +0900

    gg

--- nac-admin/src/components/HelloWorld.vue
+++ nac-admin/src/components/HelloWorld.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ userInfo? userInfo.email : 'Logout'}}</h2>
     <div class="hello">
-      <h4>아이디</h4>
-      <input type="text"></input>
-      <h4>패스워드</h4>
-      <input type="password"></input>
-      <div>
-        <button @click="setData()">가입하기</button>
+      <div class="user" v-if="userInfo">
+        <button @click="getData()">데이터가져오기</button>
+        <button @click="signout()">로그아웃</button>
+      </div>
+      <div class="user" v-if="!userInfo">
+        <button @click="signInGoogle()">구글아이디로 로그인하기</button>
+      </div>
+      <div class="add">
+        등록
+        <input type='text' v-model="name"/>
+        <input type='text' v-model="address"/>
+        <button @click="setData()">데이터등록</button>
       </div>
     </div>
     <p>
@@ -46,7 +53,34 @@
 <script lang="ts">
   import { Component, Prop, Vue } from 'vue-property-decorator';
   import firebase from 'firebase';
-
+  import firebaseui from 'firebaseui';
+  const config = {
+    apiKey: 'AIzaSyBgwBKKnFovKtJJPUKwenS-AgQ59zwuhuk',
+    authDomain: 'nac-admin.firebaseapp.com',
+    databaseURL: 'https://nac-admin.firebaseio.com',
+    projectId: 'nac-admin',
+    storageBucket: 'nac-admin.appspot.com',
+    messagingSenderId: '858334622417',
+  };
+  firebase.initializeApp(config);
+  const database = firebase.database();
+  const auth = firebase.auth();
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://nac-admin.firebaseio.com/finishSignUp',
+    // This must be true.
+    handleCodeInApp: true,
+    iOS: {
+      bundleId: 'com.nac-admin.firebaseio.ios'
+    },
+    android: {
+      packageName: 'com.nac-admin.firebaseio.android',
+      installApp: true,
+      minimumVersion: '12'
+    }
+  };
 // @Component
 // export default class HelloWorld extends Vue {
 //   @Prop() private msg!: string;
@@ -59,28 +93,76 @@
   const HelloWorld = Vue.extend({
     data(){
       return {
-        msg: 'Fucking'
-      }
+        msg: 'Test',
+        name : '',
+        address : '',
+        userInfo: firebase.auth().currentUser,
+      };
     },
     methods:{
       // `this` 때문에 리턴 타입에 어노테이션이 필요합니다.
-      SignUp():string {
-        return this.msg + ' world'
+      signInGoogle():void {
+          firebase.auth().signInWithPopup(googleAuthProvider).then((res)=>{
+            console.log('res : ', res);
+          },(error)=>{
+            console.log('res : ', error);
+          })
+      },
+      signout():void {
+        firebase.auth().signOut().then((res)=>{
+          console.log('res : ', res);
+          this.userInfo = firebase.auth().currentUser;
+        }, (error)=>{
+          console.error('Sign Out Error', error);
+        });
       },
       setData():void {
-        this.msg = this.SignUp();
+        console.log('this.name : ', this.name);
+        console.log('this.address : ', this.address);
+        if(this.userInfo){
+          firebase.database().ref('members/uid-cshee8508/').update({
+            name: this.name,
+            address: this.address,
+          },(error)=>{
+            if(error){
+              console.log('error : ', error);
+            } else {
+              console.log('success');
+            }
+          });
+        }
+      },
+      getData():void {
+        let ref = database.ref('members/uid-cshee8508');
+        console.log('ref : ', ref);
+        ref.on('value', (data) => {
+          if(data){
+            console.log('data : ', data.val());
+          }
+        });
       }
+    },
+    created(){
+      auth.onAuthStateChanged((user) => {
+        console.log('created onAuthStateChanged : ', user);
+        if(user){
+          console.log('user.providerData[0] : ', user.providerData[0]);
+        }
+        if(!this.userInfo) {
+          this.userInfo = user;
+        }
+      });
     }
   });
   export default HelloWorld;
 
 // var config = {
-//   apiKey: "AIzaSyBgwBKKnFovKtJJPUKwenS-AgQ59zwuhuk",
-//   authDomain: "nac-admin.firebaseapp.com",
-//   databaseURL: "https://nac-admin.firebaseio.com",
-//   projectId: "nac-admin",
-//   storageBucket: "nac-admin.appspot.com",
-//   messagingSenderId: "858334622417"
+//   apiKey: 'AIzaSyBgwBKKnFovKtJJPUKwenS-AgQ59zwuhuk',
+//   authDomain: 'nac-admin.firebaseapp.com',
+//   databaseURL: 'https://nac-admin.firebaseio.com',
+//   projectId: 'nac-admin',
+//   storageBucket: 'nac-admin.appspot.com',
+//   messagingSenderId: '858334622417'
 // };
 // firebase.initializeApp(config)
 // /* eslint-disable no-new */
