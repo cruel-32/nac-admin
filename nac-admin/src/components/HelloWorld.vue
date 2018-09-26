2028da701db3a3bb0f38f02d324bbb951169d358
cruel32 <cshee8508@gmail.com>
2018-09-27 01:29:02 +0900

    vue

--- nac-admin/src/components/HelloWorld.vue
+++ nac-admin/src/components/HelloWorld.vue
@@ -17,7 +17,7 @@
         <button @click="setData()">데이터등록</button>
       </div>
     </div>
-    <p>
+    <!-- <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -45,7 +45,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
